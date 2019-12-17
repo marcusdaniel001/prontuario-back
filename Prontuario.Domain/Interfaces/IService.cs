dd312b531c4bf262aec692eb8360307f2221e2be
Marcus Daniel Batista e Silva <happybrasil10@gmail.com>
2019-12-17 17:22:05 -0300

    Inicio do escopo

---
+++ Prontuario.Domain/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Prontuario.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Prontuario.Domain.Interfaces
+{
+	public interface IService<T> where T : BaseEntity
+	{
+		T Post<V>(T obj) where V : AbstractValidator<T>;
+
+		T Put<V>(T obj) where V : AbstractValidator<T>;
+
+		void Delete(int id);
+
+		T Get(int id);
+
+		IList<T> Get();
+	}
+}
